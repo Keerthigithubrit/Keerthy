f51b4ade16b57f0cc028030d512533ad83443b6d
Keerthy <155511422+Keerthigithubrit@users.noreply.github.com>
2024-03-11 13:47:27 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,9 +1,10 @@
+//JS for Navigation Links//
 $( '#navigation .navbar-nav a' ).on( 'click', function () {
 	$( '#navigation .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
 	$( this ).parent( 'li' ).addClass( 'active' );
 });
 
-/*Script for Contact-form */
+//Script for Contact-form //
 
 //const scriptURL = 'https://script.google.com/macros/s/AKfycbxQ88q7WZEkrzfmXzU_F778P2WEWAuJJyyZKWmvHI-46ZMURWK-Q_-jRrd09ZpJXwY/exec'//
 const scriptURL='https://script.google.com/macros/s/AKfycbwOGF62q9D32aKjVGC9lMv8Srrqd-RXp53Ddsa0-fxCi_a4YEAGoCJGG5FgqQ1QocqL/exec'
@@ -22,3 +23,24 @@ const scriptURL='https://script.google.com/macros/s/AKfycbwOGF62q9D32aKjVGC9lMv8
       })
       .catch(error => console.error('Error!', error.message))
   });
+
+//JS for Scroll reveal//
+
+window.addEventListener('scroll',reveal);
+function reveal()
+{
+  var reveals=document.querySelectorAll('.reveal');
+  for(var i=0;i<reveals.length;i++){
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 100;
+
+    if(revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    }
+    else{
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+
